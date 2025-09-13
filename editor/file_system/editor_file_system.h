b86b0a7a16b81bad155b5c68774acfe53501f4e6
风青山 <idleman@yeah.net>
2025-09-13 12:21:19 +0800

    Add `EditorFileSystem::_action_dir_remove()` to handle directory removal

--- editor/file_system/editor_file_system.h
+++ editor/file_system/editor_file_system.h
@@ -271,6 +271,7 @@ class EditorFileSystem : public Node {
 	List<ItemAction> scan_actions;
 
 	bool _action_file_remove(EditorFileSystemDirectory *p_dir, int p_idx, bool immediately = true);
+	bool _action_dir_remove(EditorFileSystemDirectory *p_dir, bool immediately = true);
 
 	bool _update_scan_actions();
 
